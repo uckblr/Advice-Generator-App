6de64de1b8b817c2eba2f21953f3aec1758500fe
uckblr <anggihakis@gmail.com>
2022-04-26 15:40:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const fetchAdvice = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice')
+    const data = await res.json()
+
+    document.querySelector('.title').innerHTML = `Advice #${data.slip.id}`
+    document.querySelector('.text').innerHTML = `${data.slip.advice}`
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    fetchAdvice();
+})
